1fe825ebbf91096651e0fa581ba23b4fd0fcaa35
NaniXC7 <dcabreraa2@miumg.edu.gt>
2023-02-10 23:36:14 -0600

    Se agregó el programa al repositorio

---
+++ Galletas/src/main/java/com/mycompany/galletas/Galletas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.galletas;
+
+/**
+ *
+ * @author NaniXC7
+ */
+public class Galletas {
+
+    public static void main(String[] args) {
+        new galle().setVisible(true);
+    }
+}
